d08eb6b9b5808168c97e3febc7fafa4b727cb8b5
kimgyeongmin <formin@programer.pe.kr>
2013-08-23 10:38:46 +0900

    5주차4차커밋.

--- src/test/java/net/slipp/domain/movie/MovieTest.java
+++ src/test/java/net/slipp/domain/movie/MovieTest.java
@@ -2,9 +2,14 @@ package net.slipp.domain.movie;
    
 import static org.junit.Assert.assertSame; 
     
+
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
 import org.junit.Before;
 import org.junit.Test;
-
+import java.util.Locale;
 public class MovieTest {
  
 	private Duration runningTime; 
@@ -20,8 +25,13 @@ public class MovieTest {
 	public void 식별자_일관성_보장() throws Exception {
 		
 		runningTime.setQuantity(95);
-		fee.setFeeAmout("8000");
-		fee.setFeeCurrency("KRW");
+		 
+		BigDecimal amount = new BigDecimal(8000);
+		Locale locale = Locale.getDefault();
+		Currency currency = Currency.getInstance(locale);
+		
+		fee.setFeeAmout(amount);
+		fee.setFeeCurrency(currency);
 		
 		Movie movie1 = new MovieBuilder().title("test").runningTime(runningTime).fee(fee).build();
 		Movie movie2 = new MovieBuilder().title("test").runningTime(runningTime).fee(fee).build();
